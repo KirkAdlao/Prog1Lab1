6192ff9435fb9f751b73379856a41e8802205ad6
cstuser <kirkadlao@gmail.com>
2020-01-30 14:13:10 -0500

    second commit

---
+++ MyAgeIs.java
@@ -0,0 +1,23 @@
+public class MyAgeIs {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+   int year, nextyear;
+   double distance;
+   char letter;
+   boolean state;
+
+   year = 2020;
+   nextyear = year + 1;
+
+   System.out.println(" the current year is " + year);
+   System.out.println(" the next year is " + nextyear);
+
+   distance = 2.3;
+   letter = 'a';
+   state = true; 
+
+    }
+
+}
