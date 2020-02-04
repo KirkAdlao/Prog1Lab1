769b1f86e58205bde297f8092d281daad77d256d
KirkAdlao <60393974+KirkAdlao@users.noreply.github.com>
2020-02-03 23:11:08 -0500

    initial commit

---
+++ FeetMeters.java
@@ -0,0 +1,17 @@
+import java.util.*;
+	public class FeetMeters {
+ 	 public static void main(String[] args) {
+
+ 	 	Scanner kb = new Scanner (System.in);
+ 	 	System.out.println("Please enter the unit you want to convert from meters to feet");
+ 	 	int meters = kb.nextInt();
+
+ 	 	int meter = 1;
+ 	 	int feet = 3.28084;
+ 	 	int conversion = meter * feet;
+
+ 	 	System.out.println( + feet);
+
+ 	 }
+
+}
